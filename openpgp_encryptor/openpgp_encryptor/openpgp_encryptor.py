b53005c5222a0c8a687937c63cd1cc879d042116
drzobin <me@drz.se>
2025-03-05 19:34:00 +0100

    Use ddmail_validators package

--- openpgp_encryptor/openpgp_encryptor/openpgp_encryptor.py
+++ openpgp_encryptor/openpgp_encryptor/openpgp_encryptor.py
@@ -11,6 +11,7 @@ import email.mime.application
 import email.mime.multipart
 import gnupg
 import asyncio
+import ddmail_validators.validators as validators
 import sqlalchemy as db
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, mapped_column, declarative_base, DeclarativeBase
@@ -80,13 +81,13 @@ class Ddmail_handler():
 
     def process_mail(self, sender, recipient, raw_email):
         # Validate recipient email address.
-        if self.is_email_allowed(recipient) != True:
+        if validators.is_email_allowed(recipient) != True:
             self.logging.error("validation failed for recipient email address: " + recipient)
                 
             return False
 
         # Validate from email address.
-        if self.is_email_allowed(sender) != True:
+        if validators.is_email_allowed(sender) != True:
             self.logging.error("validation failed for sender email address: " + sender)
             
             return False
@@ -106,108 +107,13 @@ class Ddmail_handler():
 
         return True
 
-    # Validate domain names. Only allow the following chars: a-z, A-Z, 0-9 and .-
-    def is_domain_allowed(self, domain):
-        if not len(domain) > 3:
-            return False
-
-        if domain.startswith('.') or domain.startswith('-'):
-            return False
-        if domain.endswith('.') or domain.endswith('-'):
-            return False
-        if '--' in domain:
-            return False
-        if '..' in domain:
-            return False
-
-        if domain.find(".") == -1:
-            return False
-
-        pattern = re.compile(r"[a-zA-Z0-9.-]")
-        for char in domain:
-            if not re.match(pattern, char):
-                return False
-
-        return True
-
-    # Validate email address. Only allow the following chars: a-z, 0-9 and @.-
-    def is_email_allowed(self, email):
-        # Check email length.
-        if not len(email) > 6:
-            return False
-        if len(email) > 256:
-            return False
-
-        if email.count('@') != 1:
-            return False
-        if email.startswith('.') or email.startswith('@') or email.startswith('-'):
-            return False
-        if email.endswith('.') or email.endswith('@') or email.endswith('-'):
-            return False
-
-        # Split email in local part and domain part example [local part]@[domain part].
-        splitted_email = email.split('@')
-        local_part = splitted_email[0]
-        domain_part = splitted_email[1]
-
-        # Validate local part of email.
-        if len(local_part) > 64:
-            return False
-        if local_part.startswith('.') or local_part.startswith('-'):
-            return False
-        if local_part.endswith('.') or local_part.endswith('-'):
-            return False
-        if '--' in local_part:
-            return False
-        if '..' in local_part:
-            return False
-
-        pattern = re.compile(r"[a-zA-Z0-9.+=_-]")
-        for char in local_part:
-            if not re.match(pattern, char):
-                return False
-
-        # Validate domain part of email.
-        if self.is_domain_allowed(domain_part) != True:
-            return False
-
-        return True
-
-    # Validate account string. Only allow the following chars: A-Z and 0-9
-    def is_account_allowed(self, account):
-        pattern = re.compile(r"[A-Z0-9]")
-
-        for char in account:
-            if not re.match(pattern, char):
-                return False
-
-        return True
-
-    # Validate openpgp public key fingerprint string. Only allow the following chars: A-Z, 0-9
-    def is_fingerprint_allowed(self, fingerprint):
-        if fingerprint == None:
-            return False
-
-        # Fingerprint string should be 40 char.
-        allowed_len = 40
-        if len(fingerprint) != allowed_len:
-            return False
-
-        # Only allow A-Z, 0-9
-        pattern = re.compile(r"[A-Z0-9]")
-        for char in fingerprint:
-            if not re.match(pattern, char):
-                return False
-
-        return True
-
     # Send email to SMTP server 127.0.0.1 port 10028
     def send_email(self, sender ,recipient, msg):
         s = smtplib.SMTP(host = self.config["DEFAULT"]["send_to_ip"], port = self.config["DEFAULT"]["send_to_port"])
         s.sendmail(sender, recipient, msg.encode("utf8"))
         s.quit()
 
-    # Check if a email is encrypted ot not.
+    # Check if a email is encrypted or not.
     def is_email_encrypted(self, raw_email):
         parsed_email = email.message_from_string(raw_email)
         look_for = ["Content-Type: multipart/encrypted","Content-Type: application/pgp-encrypted"]
@@ -262,7 +168,7 @@ class Ddmail_handler():
         r = my_session.query(Email).filter(Email.email == recipient).first()
 
         # Validate account string used as keyring filename.
-        if self.is_account_allowed(r.account.account) != True:
+        if validators.is_account_allowed(r.account.account) != True:
             self.logging.error("encrypt_email() account_keyring: " + r.account.account + " failed validation")
             my_session.close()
 
@@ -291,7 +197,7 @@ class Ddmail_handler():
         self.logging.info("encrypt_email() fingerprint: " + fingerprint)
 
         # Validate fingerprint from db.
-        if self.is_fingerprint_allowed(fingerprint) != True:
+        if validators.is_openpgp_key_fingerprint_allowed(fingerprint) != True:
             self.logging.error("encrypt_email() fingerprint: " + fingerprint + " failed validation")
             my_session.close()
 
