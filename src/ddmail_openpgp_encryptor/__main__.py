3ea5d654e71998aedc7d0730f91ad9230185c581
drzobin <me@drz.se>
2025-06-04 18:55:34 +0200

    Refactor to support public key in db

--- src/ddmail_openpgp_encryptor/__main__.py
+++ src/ddmail_openpgp_encryptor/__main__.py
@@ -2,14 +2,16 @@ import sys
 import os
 import argparse
 import logging
+import logging.handlers
 import asyncio
-import configparser
+import toml
+from logging import FileHandler
 from aiosmtpd.controller import Controller
 from ddmail_openpgp_encryptor.ddmail_handler import Ddmail_handler
 
-async def run(loop, logging, config):
-    handler = Ddmail_handler(logging, config)
-    controller = Controller(handler, hostname=config["DEFAULT"]["listen_on_ip"], port=int(config["DEFAULT"]["listen_on_port"]))
+async def run(loop, logger, config):
+    handler = Ddmail_handler(logger, config)
+    controller = Controller(handler, hostname=config["LISTEN_ON_IP"], port=int(config["LISTEN_ON_PORT"]))
     controller.start()
 
     # Run forever.
@@ -19,10 +21,6 @@ async def run(loop, logging, config):
         controller.stop()
 
 def main():
-    # Configure logging.
-    logging.basicConfig(filename="/var/log/ddmail_openpgp_encryptor.log", format='%(asctime)s: %(levelname)s: %(message)s', level=logging.INFO)
-    logging.info("openpgp_encryptor starting")
-    
     # Get arguments from args.
     parser = argparse.ArgumentParser(description="Encrypt email with OpenPGP for ddmail service.")
     parser.add_argument('--config-file', type=str, help='Full path to config file.', required=True)
@@ -30,16 +28,49 @@ def main():
 
     # Check that config file exists and is a file.
     if not os.path.isfile(args.config_file):
-        logging.error("config file does not exist or is not a file.")
+        print("ERROR: config file does not exist or is not a file.")
         sys.exit(1)
 
-    # Import config file.
-    config = configparser.ConfigParser()
-    conf_file = args.config_file
-    config.read(conf_file)
+    # Parse toml config file.
+    with open(args.config_file, 'r') as f:
+        toml_config = toml.load(f)
+
+    # Setup logging.
+    logger = logging.getLogger(__name__)
+
+    formatter = logging.Formatter(
+        "{asctime} ddmail_openpgp_encryptor {levelname} in {module} {funcName} {lineno}: {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M",
+        )
+
+    if toml_config["LOGGING"]["LOG_TO_CONSOLE"] == True:
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+    if toml_config["LOGGING"]["LOG_TO_FILE"] == True:
+        file_handler = logging.FileHandler(toml_config["LOGGING"]["LOGFILE"], mode="a", encoding="utf-8")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    if toml_config["LOGGING"]["LOG_TO_SYSLOG"] == True:
+        syslog_handler = logging.handlers.SysLogHandler(address = toml_config["LOGGING"]["SYSLOG_SERVER"])
+        syslog_handler.setFormatter(formatter)
+        logger.addHandler(syslog_handler)
+
+    # Set loglevel.
+    if toml_config["LOGGING"]["LOGLEVEL"] == "DEBUG":
+        logger.setLevel(logging.DEBUG)
+    elif toml_config["LOGGING"]["LOGLEVEL"] == "INFO":
+        logger.setLevel(logging.INFO)
+    elif toml_config["LOGGING"]["LOGLEVEL"] == "WARNING":
+        logger.setLevel(logging.WARNING)
+    elif toml_config["LOGGING"]["LOGLEVEL"] == "ERROR":
+        logger.setLevel(logging.ERROR)
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(loop, logging, config))
+    loop.run_until_complete(run(loop, logger, toml_config))
 
 
 if __name__ == "__main__":
